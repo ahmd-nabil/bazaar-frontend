16330709fadf3abb58c0cf15c6cb9ecad12dd8e7
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-18 15:14:34 +0300

    added validators (with chatgpt ;)

--- src/app/components/checkout-form/checkout-form.component.ts
+++ src/app/components/checkout-form/checkout-form.component.ts
@@ -22,23 +22,23 @@ export class CheckoutFormComponent implements OnInit{
         email: ['', [Validators.required, Validators.email]]
       }),
       creditCard: this.formBuilder.group({
-        cardNumber: ['', Validators.required],
-        cardExpiry: ['', Validators.required],
-        cardCVC: ['', Validators.required]
+        cardNumber: ['', [Validators.required, Validators.pattern(/^\d{16}$/)]],
+        cardExpiry: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]],
+        cardCVC: ['', [Validators.required, Validators.pattern(/^\d{3}$/)]]
       }),
       shippingAddress: this.formBuilder.group({
         country: ['', Validators.required],
         street: ['', Validators.required],
         city: ['', Validators.required],
         state: ['', Validators.required],
-        zipcode: ['', Validators.required]
+        zipcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]]
       }),
       billingAddress: this.formBuilder.group({
         billingCountry: ['', Validators.required],
         billingStreet: ['', Validators.required],
         billingCity: ['', Validators.required],
         billingState: ['', Validators.required],
-        billingZipcode: ['', Validators.required],
+        billingZipcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
         sameAsShipping: false
       })
     });
@@ -47,17 +47,17 @@ export class CheckoutFormComponent implements OnInit{
   onSameAsShippingChange(event: any) {
     let same: boolean = event.target.checked == null ? false : event.target.checked;
     if(same) {
-      this.checkoutForm.get('billingAddress')?.get('billingCountry')?.setValue(this.checkoutForm.get('shippingAddress')?.get('country')?.getRawValue());
-      this.checkoutForm.get('billingAddress')?.get('billingStreet')?.setValue(this.checkoutForm.get('shippingAddress')?.get('street')?.getRawValue());
-      this.checkoutForm.get('billingAddress')?.get('billingCity')?.setValue(this.checkoutForm.get('shippingAddress')?.get('city')?.getRawValue());
-      this.checkoutForm.get('billingAddress')?.get('billingState')?.setValue(this.checkoutForm.get('shippingAddress')?.get('state')?.getRawValue());
-      this.checkoutForm.get('billingAddress')?.get('billingZipcode')?.setValue(this.checkoutForm.get('shippingAddress')?.get('zipcode')?.getRawValue());
+      this.checkoutForm.get('billingAddress.billingCountry')?.setValue(this.checkoutForm.get('shippingAddress.country')?.getRawValue());
+      this.checkoutForm.get('billingAddress.billingStreet')?.setValue(this.checkoutForm.get('shippingAddress.street')?.getRawValue());
+      this.checkoutForm.get('billingAddress.billingCity')?.setValue(this.checkoutForm.get('shippingAddress.city')?.getRawValue());
+      this.checkoutForm.get('billingAddress.billingState')?.setValue(this.checkoutForm.get('shippingAddress.state')?.getRawValue());
+      this.checkoutForm.get('billingAddress.billingZipcode')?.setValue(this.checkoutForm.get('shippingAddress.zipcode')?.getRawValue());
     } else {
-      this.checkoutForm.get('billingAddress')?.get('billingCountry')?.setValue('');
-      this.checkoutForm.get('billingAddress')?.get('billingStreet')?.setValue('');
-      this.checkoutForm.get('billingAddress')?.get('billingCity')?.setValue('');
-      this.checkoutForm.get('billingAddress')?.get('billingState')?.setValue('');
-      this.checkoutForm.get('billingAddress')?.get('billingZipcode')?.setValue('');
+      this.checkoutForm.get('billingAddress.billingCountry')?.setValue('');
+      this.checkoutForm.get('billingAddress.billingStreet')?.setValue('');
+      this.checkoutForm.get('billingAddress.billingCity')?.setValue('');
+      this.checkoutForm.get('billingAddress.billingState')?.setValue('');
+      this.checkoutForm.get('billingAddress.billingZipcode')?.setValue('');
     }
   }
 
