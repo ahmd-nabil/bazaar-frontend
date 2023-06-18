23b7b44be7aac1cb33b59815ec41d762b2fb7cd6
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-18 14:07:53 +0300

    defining route to checkout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CategoriesListComponent } from './components/categories-list/categories
 import { ProductsComponent } from './components/products/products.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path:'products', component: ProductsComponent },
   { path:'products/:productId', component: ProductDetailsComponent },
   { path: 'cart', component: CartDetailsComponent},
+  { path: 'checkout', component: CheckoutFormComponent},
   { path:"**", component: MainComponent }
 ];
 
