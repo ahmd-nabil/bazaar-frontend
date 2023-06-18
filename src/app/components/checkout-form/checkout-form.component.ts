bf0df73f84db6ab844f73dbf886cb3a4539f19fc
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-19 02:51:09 +0300

    refactored naming for billing address

--- src/app/components/checkout-form/checkout-form.component.ts
+++ src/app/components/checkout-form/checkout-form.component.ts
@@ -34,11 +34,11 @@ export class CheckoutFormComponent implements OnInit{
         zipcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]]
       }),
       billingAddress: this.formBuilder.group({
-        billingCountry: ['', Validators.required],
-        billingStreet: ['', Validators.required],
-        billingCity: ['', Validators.required],
-        billingState: ['', Validators.required],
-        billingZipcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
+        country: ['', Validators.required],
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        zipcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
         sameAsShipping: false
       })
     });
@@ -47,17 +47,17 @@ export class CheckoutFormComponent implements OnInit{
   onSameAsShippingChange(event: any) {
     let same: boolean = event.target.checked == null ? false : event.target.checked;
     if(same) {
-      this.checkoutForm.get('billingAddress.billingCountry')?.setValue(this.checkoutForm.get('shippingAddress.country')?.getRawValue());
-      this.checkoutForm.get('billingAddress.billingStreet')?.setValue(this.checkoutForm.get('shippingAddress.street')?.getRawValue());
-      this.checkoutForm.get('billingAddress.billingCity')?.setValue(this.checkoutForm.get('shippingAddress.city')?.getRawValue());
-      this.checkoutForm.get('billingAddress.billingState')?.setValue(this.checkoutForm.get('shippingAddress.state')?.getRawValue());
-      this.checkoutForm.get('billingAddress.billingZipcode')?.setValue(this.checkoutForm.get('shippingAddress.zipcode')?.getRawValue());
+      this.checkoutForm.get('billingAddress.country')?.setValue(this.checkoutForm.get('shippingAddress.country')?.getRawValue());
+      this.checkoutForm.get('billingAddress.street')?.setValue(this.checkoutForm.get('shippingAddress.street')?.getRawValue());
+      this.checkoutForm.get('billingAddress.city')?.setValue(this.checkoutForm.get('shippingAddress.city')?.getRawValue());
+      this.checkoutForm.get('billingAddress.state')?.setValue(this.checkoutForm.get('shippingAddress.state')?.getRawValue());
+      this.checkoutForm.get('billingAddress.zipcode')?.setValue(this.checkoutForm.get('shippingAddress.zipcode')?.getRawValue());
     } else {
-      this.checkoutForm.get('billingAddress.billingCountry')?.setValue('');
-      this.checkoutForm.get('billingAddress.billingStreet')?.setValue('');
-      this.checkoutForm.get('billingAddress.billingCity')?.setValue('');
-      this.checkoutForm.get('billingAddress.billingState')?.setValue('');
-      this.checkoutForm.get('billingAddress.billingZipcode')?.setValue('');
+      this.checkoutForm.get('billingAddress.country')?.setValue('');
+      this.checkoutForm.get('billingAddress.street')?.setValue('');
+      this.checkoutForm.get('billingAddress.city')?.setValue('');
+      this.checkoutForm.get('billingAddress.state')?.setValue('');
+      this.checkoutForm.get('billingAddress.zipcode')?.setValue('');
     }
   }
 
