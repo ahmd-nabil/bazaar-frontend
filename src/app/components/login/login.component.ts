cd7cc13bdeeee3dabe6cfffe4fbe2eea522a885d
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-22 06:43:32 +0300

    using okta-siginin-widget for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
+import OktaSignIn from '@okta/okta-signin-widget';
+import oktaConfig from 'src/app/config/okta-config';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  oktaSignIn: any;
 
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+    this.oktaSignIn = new OktaSignIn({
+      logo: '/assets/images/logo_transparent.png',
+      baseUrl: oktaConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: oktaConfig.oidc.clientId,
+      redirectUri: oktaConfig.oidc.redirectUri,
+      useInteractionCodeFlow: oktaConfig.oidc.useInteractionCodeFlow,
+      useClassicEngine: oktaConfig.oidc.useClassicEngine,
+      authParams: {
+        pkce: true,
+        issuer: oktaConfig.oidc.issuer,
+        scopes: oktaConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.oktaSignIn.remove();
+    this.oktaSignIn.renderEl({
+      el: '#okta-signin-widget'
+    }, (response: any) => {
+      if(response.status == 'SUCCESS') {
+        this.oktaAuth.signInWithRedirect();
+      }
+    },
+    (error: any) => {
+      throw error
+    }
+    )
+  }
 }
