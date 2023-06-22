9c842e428ce8abeeca5e900f8afd6c12727d0c0c
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-22 08:19:27 +0300

    customized status based on authentication

--- src/app/components/navbar/login-status/login-status.component.ts
+++ src/app/components/navbar/login-status/login-status.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { OKTA_AUTH, OktaAuthStateService } from '@okta/okta-angular';
+import OktaAuth, { UserClaims } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-login-status',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-status.component.css']
 })
 export class LoginStatusComponent {
+  isAuthenticated = false;
+  userClaims: UserClaims;
+  
+  constructor(
+    private oktaAuthStateService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth
+    ) 
+    {
+    this.oktaAuthStateService.authState$.subscribe(
+      authState => {
+        this.isAuthenticated = authState.isAuthenticated;
+        if(this.isAuthenticated) {
+          this.oktaAuth.getUser().then(result => this.userClaims = result);
+        }
+      }
+    );
+  }
+
+  logout() {
+    this.oktaAuth.signOut();
+  }
+
 
 }
