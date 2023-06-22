6748dd6d815ec896fc05eff83faa4f8c40d793d6
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-22 09:41:15 +0300

    protecting myprofile route with OktaAuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injector, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -7,7 +7,14 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
 import { LoginComponent } from './components/login/login.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
+import { MyprofileComponent } from './components/myprofile/myprofile.component';
+import OktaAuth from '@okta/okta-auth-js';
+
+function sendToLogin(oktaAuth: OktaAuth, injector: Injector) {
+  let router = injector.get(Router);
+  router.navigate(['/login']);
+}
 
 const routes: Routes = [
   { path: '', component: MainComponent },
@@ -18,6 +25,7 @@ const routes: Routes = [
   { path: 'checkout', component: CheckoutFormComponent},
   { path: 'login/callback', component: OktaCallbackComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'myprofile', component: MyprofileComponent, canActivate: [ OktaAuthGuard ], data:{onAuthRequired: sendToLogin} },
   { path:"**", component: MainComponent }
 ];
 
